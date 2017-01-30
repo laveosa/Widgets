3c2f47d8fb859e90cada6bff1aad5fffabee3928
Nikolay <laveosa@yahoo.com>
2017-01-30 22:53:08 +0200

    second tab created!!!
    colorfull tab witch saving text in local storage

--- js/main_brain.js
+++ js/main_brain.js
@@ -13,7 +13,7 @@
 
 		function setAnim(){
 			var text = new Array();
-			text.push('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quam, culpa rerum beatae illum laudantium. At quisquam aperiam pariatur. Soluta.');
+			text.push('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quam, culpa rerum beatae illum laudantium. At quisquam aperiam pariatur. Soluta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quam, culpa rerum beatae illum laudantium. At quisquam aperiam pariatur. Soluta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quam, culpa rerum beatae illum laudantium. At quisquam aperiam pariatur. Soluta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero quam, culpa rerum beatae illum laudantium. At quisquam aperiam pariatur. Soluta. ');
 			text.push("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ex eius veritatis ducimus saepe, commodi, quasi autem voluptas modi asperiores vero nisi id! Eius minus blanditiis nulla fugiat, sint veniam eveniet! Saepe reiciendis labore reprehenderit ut obcaecati alias placeat vitae!");
 			text.push("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum suscipit, neque saepe. Ducimus totam, ipsam tempore, debitis unde amet. Eos quos eius minus nulla incidunt numquam aut ratione officiis accusamus officia vero at, quo ipsa nam sunt quidem praesentium culpa consectetur, veritatis eveniet voluptas illo! Consectetur soluta reprehenderit delectus est officiis libero itaque optio harum quas. Maiores id reiciendis unde!");
 			text.push('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur ea sed a distinctio necessitatibus quidem labore assumenda doloribus nisi impedit laborum modi commodi numquam, fuga veritatis aliquid animi ad eos atque hic ab alias. At praesentium fugiat distinctio iure excepturi magnam, ex omnis numquam, eius nihil suscipit velit dolorum non! Exercitationem magni repellat dolorem soluta nihil laborum earum illum omnis veniam id eos asperiores sapiente modi possimus, harum repudiandae vel, quam suscipit voluptatibus nemo vero error, doloribus alias! Reiciendis, et voluptates ipsam exercitationem soluta laborum mollitia nesciunt dolore neque rerum totam, ab corporis natus libero eius dolorum. Ipsam, maxime officiis rem animi natus, eos facere, eveniet, minima atque explicabo optio! Dignissimos vero ut ratione accusantium molestiae impedit consequatur maiores enim!');
@@ -40,7 +40,6 @@
 			for (var i = 1; i <= elems.length; i++) {
 				var path = "#tab"+i+" span";
 				var obj = document.querySelector(path);
-				console.log(obj);
 				obj.parentNode.removeChild(obj);
 			}
 			setAnim();
@@ -50,7 +49,44 @@
 })();
 
 
+(function(){
+	window.addEventListener("load", function(){
+		$(".bookMark").on("click", function(){
+			$(".bookMark").css({
+				top: "30px"
+			});
+			$(this).css({
+				top: "6px"
+			});
 
+			$(".page").css({height: 0, display: "none"});
+			var page = document.getElementById(this.getAttribute("data-tail"));
+			
+			$(page).css({
+				height: "100%",
+				display: "block"
+			});
+
+		});
+
+		$(".textPage").on("input", function(){
+			var key = this.parentNode.id;
+			var message = this.value;
+			localStorage.setItem(key, message);
+
+		});
+
+		$(".bookMark").eq(0).click();
+		getText();
+
+		function getText(){
+			var elems = $(".page");
+			for (var i = 0; i < elems.length; i++) {
+				$(elems[i]).children().val(localStorage.getItem(elems[i].id));
+			}
+		}
+	});
+})();
 
 
 
